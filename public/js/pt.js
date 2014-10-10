b9fec8e20d8e5d3e11093e6dc7569df98a605a43
David Karam <david@posttool.com>
2014-10-10 10:39:24 -0700

    more

---
+++ public/js/pt.js
@@ -0,0 +1,27 @@
+var y0 = 0;
+var y1 = 210;
+$( window ).scroll(function() {
+  var y3 = $(window).height()-100;
+  var y2 = y3 - 100;
+  var st = $(window).scrollTop();
+  $('.col').each(function( index, element ){
+    var $el = $(element);
+    var p = $el.position() ;
+    var h = $el.height();
+    var y = p.top - st;
+    var yh = y+h;
+    var o = 1;
+    if (yh<y1) {
+      if (yh<y0)
+        o = 0;
+      else
+        o = 1-(y1-yh)/(y1-y0);
+    } else if (y>y2) {
+      if (y>y3)
+        o = 0;
+      else
+        o = 1-(y-y2)/(y3-y2);
+    }
+    $el.css({opacity: o});
+  })
+});
