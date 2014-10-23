cc878fd449fecb46a78efb95fec2fe1ff1851b39
David Karam <david@posttool.com>
2014-10-23 13:15:19 -0700

    to the lib

--- cms/index.js
+++ cms/index.js
@@ -7,42 +7,14 @@ exports = module.exports = {
   permissions: require('./permission')
 };
 
-var current = require('../../currentcms');
-var utils = require('../../currentcms/lib/utils');
-
-// node cms admin ... & node cms start ... TODO could be baked into library
 if (process.argv.length > 2 && process.argv[2] == 'admin') {
-  //console.log(module.exports)
-  var mongoose = require('mongoose');
-  var prompt = require('prompt');
-  prompt.message = "create admin > ".cyan;
-  prompt.delimiter = "".grey;
-  var prompt_schema = {
-    properties: {
-      name: {required: true},
-      email: {required: true},
-      password: {hidden: true},
-      confirm: {hidden: true}
-    }
-  };
-  prompt.start();
-  prompt.get(prompt_schema, function (err, result) {
-    if (result.password != result.confirm)
-      throw new Error('Password mismatch!');
-    var cms = new current.Cms(module.exports);
-    var User = cms.meta.model('User');
-    utils.save_user(new User(), {name: result.name, email: result.email, email_verified: true,
-      password: result.password, active: true, admin: true }, function (err, user) {
-      if (err)
-        throw new Error(err);
-      console.log('Complete', user);
-    });
-  });
-} else if (process.argv.length > 2 && process.argv[2] == 'start') {
-  var app = require('express')();
-  var cms = new current.Cms(module.exports);
-  app.use(cms.app);
-  // app.use(express.static(__dirname + '/public')); // if we have custom fields
-  app.listen(cms.config.serverPort);
+  var utils = require('../../currentcms/lib/utils');
+  switch (process.argv[2]) {
+    case 'admin':
+      utils.create_admin(module.exports);
+      break;
+    case 'start':
+      utils.start_server(module.exports);
+      break;
+  }
 }
-
